014abf5ac6ac92a2ab974af90d360e7b49bdb145
cdbst <parkhyeonwook@gmail.com>
2021-10-04 02:37:21 +0900

    abck cookie 가져 와지는 동작까지 확인됨.

--- app.js
+++ app.js
@@ -6,12 +6,22 @@ const express = require('express');
 const body_parser = require('body-parser');
 const app = express();
 app.use(body_parser.json());
+app.use(body_parser.text());
 app.use(express.static('public'));
+app.use(body_parser.urlencoded({ extended: true }));
+
+const root_cas = require('ssl-root-cas').create();
+const https = require('https');
+const http = require('http');
+
+https.globalAgent.options.ca = root_cas;
+
 const port = 3000;
 
-let g_cookies = ['NikeCookie=ok;', 'social_type=comlogin;'];
+let g_cookies = ['NikeCookie=ok;', 'social_type=comlogin;', 'anonymousId=9B4FE976FFE05E23E447162D70BED0AB;'];
 let g_csrfToken = undefined;
 let g_customer_id = undefined;
+let g_ping_url = undefined;
 
 app.get('/akam_sensor_gen.js_modified.js', (req, res) =>{
     res.sendFile(__dirname + '\\akam_sensor_gen.js_modified.js');
@@ -38,13 +48,64 @@ app.get('/test', (req, res) =>{
 });
 
 app.post('/sensor_data', (req, res) =>{
-    var body = req.body;
+    get_akam_cookies(req.body);
+    res.send(JSON.stringify({'msg' :'POST request to the homepage'}));
+});
+
+app.post('/sensor_data_test', (req, res) =>{
+    console.log(req.headers);
     res.send(JSON.stringify({'msg' :'POST request to the homepage'}));
 });
 
 app.listen(port, ()=>{
     console.log('web server on');
 });
+
+function get_akam_cookies(sensor_data){
+    
+
+    let data_len = JSON.stringify(sensor_data).length;
+    let _cookies = g_cookies.join(' ');
+    var _test_cookies = 'anonymousId=9B4FE976FFE05E23E447162D70BED0AB';
+
+    let config = {
+        headers: {
+            "authority": 'www.nike.com',
+            // "path" : _path,
+            // "method": "POST",
+            "accept": "*/*",
+            "accept-language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
+            "cache-control": "no-cache",
+            "content-type": "text/plain;charset=UTF-8",
+            "pragma": "no-cache",
+            "sec-ch-ua": "\"Chromium\";v=\"90\", \" Not A;Brand\";v=\"99\", \"Whale\";v=\"2\"",
+            "sec-ch-ua-mobile": "?0",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-origin",
+            "cookie": _cookies,
+            "origin": 'https://www.nike.com',
+            "referer": "https://www.nike.com/kr/ko_kr",
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.232 Whale/2.10.124.26 Safari/537.36',
+            'content-length': data_len
+            // 'connection' : 'keep-alive',
+            // 'dnt': '1'
+        }
+    }
+
+    axios.post('https://www.nike.com' + g_ping_url, sensor_data, config)
+    .then(res => {
+
+        if(res.status == 201){
+            console.log(res.data);
+        }else{
+            console.log('req fail - status code : ' + res.status);
+        }
+    })
+    .catch(error => {
+        console.error(error)
+    });
+}
 
 function do_login(id, pwd, cb) {
 
@@ -61,6 +122,7 @@ function do_login(id, pwd, cb) {
     }
 
     let data_len = JSON.stringify(data).length;
+    let _cookies = g_cookies.join(' ');
 
     let config = {
         headers: {
@@ -71,7 +133,7 @@ function do_login(id, pwd, cb) {
             // 'content-length': data_len,
             'content-type':'application/x-www-form-urlencoded; charset=UTF-8',
             'content-length': data_len,
-            'cookie': g_cookies.join(' '),
+            'cookie': _cookies,
             'origin':'https://www.nike.com',
             'pragma':'no-cache',
             'referer':'https://www.nike.com/kr/ko_kr/',
@@ -85,7 +147,6 @@ function do_login(id, pwd, cb) {
         }
     }
 
-
     axios.post('https://www.nike.com/kr/ko_kr/login_post.htm', data, config)
     .then(res => {
 
@@ -94,8 +155,6 @@ function do_login(id, pwd, cb) {
         }else{
             console.log('req fail - status code : ' + res.status);
         }
-        
-        read_stock_tiker();
     })
     .catch(error => {
         console.error(error)
@@ -121,7 +180,7 @@ function access_main_page(cb){
                 g_csrfToken = l_csrfToken_el[0].attribs.value;   
             }
 
-            let scripts = $('script:not([src])').get()
+            let scripts = $('script:not([src])').get();
             let data_script = undefined;
             for(var i = 0 ; i < scripts.length; i++){
                 
@@ -139,8 +198,22 @@ function access_main_page(cb){
 
             let setting_data = data_script.children[0].data;
             g_customer_id = setting_data.split('ID :')[1].split(',')[0].trim();
-            //g_cookies.push('USERID=' + g_customer_id +';');
-            cb();
+            g_cookies.push('USERID=' + g_customer_id +';');
+
+
+            scripts = $('script').get();
+
+            for(var i = 0; i < scripts.length; i++){
+
+                let script = scripts[i];
+                if(('src' in script.attribs) == false) continue;
+
+                if((script.attribs.src.includes('https://'))) continue;
+                if((script.attribs.src.startsWith('//'))) continue;
+                console.log(script.attribs.src);
+                g_ping_url = script.attribs.src;
+                break;
+            }
 
         }else{
             console.log('req fail - status code : ' + res.status);
@@ -152,12 +225,8 @@ function access_main_page(cb){
     });
 }
 
-function ak_ping(){
-    
-}
-
-// access_main_page(function(){
-//     do_login(id, pwd);
-// });
+access_main_page(function(){
+    //do_login(id, pwd);
+});
 
 
