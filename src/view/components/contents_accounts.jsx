96cc163dcfdca9d9c70a533400832fca559c2938
cdbst <parkhyeonwook@gmail.com>
2021-10-07 00:18:59 +0900

    Account 기본적인 UI 구성 작업 완료.

--- src/view/components/contents_accounts.jsx
+++ src/view/components/contents_accounts.jsx
@@ -31,50 +31,19 @@ class ContentsAccounts extends React.Component {
                             <td>pakd123@naver.com</td>
                             <td>박현욱</td>
                             <td>Valid</td>
-                            <td>Modify/Remove Buttions</td>
-                        </tr>
-                        <tr>
-                            <td>pakd123@naver.com</td>
-                            <td>박현욱</td>
-                            <td>Valid</td>
-                            <td>Modify/Remove Buttions</td>
-                        </tr>
-                        <tr>
-                            <td>pakd123@naver.com</td>
-                            <td>박현욱</td>
-                            <td>Valid</td>
-                            <td>Modify/Remove Buttions</td>
-                        </tr>
-                        <tr>
-                            <td>pakd123@naver.com</td>
-                            <td>박현욱</td>
-                            <td>Valid</td>
-                            <td>Modify/Remove Buttions</td>
-                        </tr>
-                        <tr>
-                            <td>pakd123@naver.com</td>
-                            <td>박현욱</td>
-                            <td>Valid</td>
-                            <td>Modify/Remove Buttions</td>
-                        </tr>
-                        <tr>
-                            <td>pakd123@naver.com</td>
-                            <td>박현욱</td>
-                            <td>Valid</td>
-                            <td>Modify/Remove Buttions</td>
-                        </tr>
-                        <tr>
-                            <td>pakd123@naver.com</td>
-                            <td>박현욱</td>
-                            <td>Valid</td>
-                            <td>Modify/Remove Buttions</td>
-                        </tr>
-
-                        <tr>
-                            <td>pakd123@naver.com</td>
-                            <td>박현욱</td>
-                            <td>Valid</td>
-                            <td>Modify/Remove Buttions</td>
+                            <td>
+                                <div>
+                                    <div className="float-start button-wrapper">
+                                        <button type="button" className="btn btn-success"><img src="./res/door-open-fill.svg" style={{width:24, height:24}}/></button>
+                                    </div>
+                                    <div className="float-start button-wrapper">
+                                        <button type="button" className="btn btn-light"><img src="./res/pencil-square.svg" style={{width:24, height:24}}/></button>
+                                    </div>
+                                    <div className="float-start button-wrapper">
+                                    <button type="button" className="btn btn-danger"><img src="./res/trash-fill.svg" style={{width:24, height:24}}/></button>
+                                    </div>
+                                </div>
+                            </td>
                         </tr>
                     </tbody>
                 </table>
