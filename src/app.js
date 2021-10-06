f91539e7c28a6bdfe2b32c7d9984b50b522ed974
cdbst <parkhyeonwook@gmail.com>
2021-10-06 13:30:32 +0900

    .

--- src/app.js
+++ src/app.js
@@ -1,30 +1,32 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
-function createWindow () {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-    }
-  })
-
-  win.loadFile(path.join(__dirname, 'index.html'))
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        nodeIntegration: true,
+        frame: false,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+        },
+    });
+    win.webContents.openDevTools();
+    win.loadFile(path.join(__dirname, "index.html"));
 }
 
 app.whenReady().then(() => {
-  createWindow()
+    createWindow();
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
     }
-  })
-})
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+});
