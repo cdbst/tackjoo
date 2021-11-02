a56f3d7036aef5390a3c0971178566c63251a59e
cdbst <parkhyeonwook@gmail.com>
2021-11-02 23:34:50 +0900

    request_gate.jsx 추가

---
+++ src/view/components/request_gate.jsx
@@ -0,0 +1,31 @@
+/**
+ * Election api request gate for prevent duplicated request within very shorten times.
+ */
+class RequestGate{
+    constructor(){
+        this.subscribe = this.subscribe.bind(this);
+        this.notify = this.notify.bind(this);
+
+        this.queue = {};
+    }
+
+    subscribe(_id, subscriber){
+        if(_id in this.queue == false){
+            this.queue[_id] = [];
+        }
+
+        this.queue[_id].push(subscriber);
+    }
+
+    notify(_id, error, product_info){
+        this.queue[_id].forEach((subscriber) =>{
+            subscriber(error, product_info);
+        });
+        this.queue[_id] = [];
+    }
+
+    isRequstOnPending(_id){
+        if(_id in this.queue == false) return false;
+        return this.queue[_id].length > 0;
+    }
+}
