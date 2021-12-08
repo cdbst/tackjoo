0edad03aa1c17d5a7d8377518389b620f616abfe
cdbst <parkhyeonwook@gmail.com>
2021-12-08 22:49:46 +0900

    user_file_path.js 위치 변경 settings 저장시 알람 메시지 추가.

---
+++ src/utils/user_file_path.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+const USER_FILE_PATH = {
+    USER_INFO :  path.join(__dirname, '..', 'res', 'data', 'user_info.dat'),
+    BILLING_INFO : path.join(__dirname, '..', 'res', 'data', 'billing_info.dat'),
+    PROXY_INFO : path.join(__dirname, '..', 'res', 'data', 'proxy_info.dat'),
+    SETTINGS_INFO : path.join(__dirname, '..', 'res', 'data', 'settings.dat')
+};
+
+module.exports.USER_FILE_PATH = USER_FILE_PATH;
