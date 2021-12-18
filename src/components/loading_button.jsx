5bce7323e0c725ea6194889cc3e7b77f90f2d17a
cdbst <parkhyeonwook@gmail.com>
2021-12-18 13:42:26 +0900

    Loading Button 컴포넌트 추가.

---
+++ src/components/loading_button.jsx
@@ -0,0 +1,30 @@
+class LaodingButton extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.__loading_img_src = './res/img/tail-spin.svg';
+        this.state = {
+            img_src : this.props.img_src
+        }
+        this.__ref_btn = React.createRef();
+    }
+
+    setLoginStatus(status){
+        
+        this.__ref_btn.current.disabled = status;
+        
+        this.setState(_ => ({
+            img_src : status ? this.__loading_img_src : this.props.img_src
+        }));
+    }
+
+    render(){
+        return(
+            <button ref={this.__ref_btn} type="button" className={"btn " + this.props.btn_class} onClick={()=>{this.props.h_on_click()}}>
+                <img src={this.state.img_src} style={{width:24, height:24}}/>
+                {this.props.btn_label}
+            </button>
+        );
+    }
+}
