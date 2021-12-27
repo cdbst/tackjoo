09ac2ec22e6ed775ee92879774a38e3ce45d8dc7
cdbst <parkhyeonwook@gmail.com>
2021-12-27 18:26:08 +0900

    update ui 관련 코드 추가.

--- src/app.js
+++ src/app.js
@@ -42,17 +42,22 @@ function create_window() {
     }
 }
 
-
 /**
  * 앱 업데이트 기능
  * 
  */
-
 let update_win = undefined;
 
-function sendStatusToWindow(text) {
-    update_win.webContents.send('message', text);
+function send_message_to_update_win(message) {
+    if(update_win === undefined) return;
+    update_win.webContents.send('message', message);
 }
+
+function send_progess_to_update_win(progress_percent) {
+    if(update_win === undefined) return;
+    update_win.webContents.send('progress', progress_percent);
+}
+
 function create_update_window() {
     update_win = new BrowserWindow({
         width: 400,
@@ -61,45 +66,42 @@ function create_update_window() {
         minHeight : 500,
         titleBarStyle: "hidden",
         webPreferences: {
-            //devTools: false,
-            //sandbox: true,
             nodeIntegration: true,
             contextIsolation: false,
             enableRemoteModule: true,
         },
         offscreen : true,
-        //resizable: false
+        resizable: false
     });
     update_win.webContents.openDevTools();
     update_win.on('closed', () => {
         update_win = null;
     });
-    update_win.loadURL(`file://${__dirname}/update.html#v${app.getVersion()}`);
+    update_win.loadURL(path.join(__dirname, "update.html"));
     return update_win;
 }
 
 autoUpdater.on('checking-for-update', () => {
-    sendStatusToWindow('Checking for update...');
+    send_message_to_update_win('업데이트 확인 중..');
 })
 autoUpdater.on('update-available', (info) => {
-    sendStatusToWindow('Update available. ' + info);
+    send_message_to_update_win(`새로운 버전이 확인됐습니다. -> ${info.version}`);
 })
 autoUpdater.on('update-not-available', (info) => {
-    sendStatusToWindow('Update not available. ' + info );
     update_win.close();
     create_window();
 })
 autoUpdater.on('error', (err) => {
-    sendStatusToWindow('Error in auto-updater. ' + err);
+    send_message_to_update_win('업데이트에 실패 했습니다. 수동 다운로드 : https://github.com/cdbst/sbkr_release/releases');
 })
-autoUpdater.on('download-progress', (progressObj) => {
-    let log_message = "Download speed: " + progressObj.bytesPerSecond;
-    log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
-    sendStatusToWindow(log_message);
+autoUpdater.on('download-progress', (progress_obj) => {
+    let log_message = "다운로드 속도: " + progress_obj.bytesPerSecond;
+    log_message = log_message + ' (' + progress_obj.transferred + "/" + progress_obj.total + ')';
+    send_message_to_update_win(log_message);
+    send_progess_to_update_win(progress_obj.percent);
 })
 autoUpdater.on('update-downloaded', (info) => {
-    sendStatusToWindow('Update downloaded');
+    send_message_to_update_win('다운로드 완료. 다시 시작하는 중..');
     autoUpdater.quitAndInstall(false, true);
 });
 
@@ -109,7 +111,6 @@ app.whenReady().then(() => {
     create_update_window();
     autoUpdater.checkForUpdatesAndNotify();
     
-
     //for mac platform
     app.on("activate", () => {
         if (BrowserWindow.getAllWindows().length === 0) {
