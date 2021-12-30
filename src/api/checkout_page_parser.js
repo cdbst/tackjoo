13b7f0267345581115d122cb6544c9f0109bc966
cdbst <parkhyeonwook@gmail.com>
2021-12-31 00:04:18 +0900

    Revert "특정 환경에서 결제를 진행하지 못하는 문제 수정. (#104)"
    This reverts commit bf42c98d6b9308fba238ba63ee9a0e37833158ec.

--- src/api/checkout_page_parser.js
+++ src/api/checkout_page_parser.js
@@ -10,19 +10,11 @@ function parse_kakaopay_prepare_payload_from_checkout_page($){
     let name = parser_common.get_data_tag_value($, 'a', 'data-eng-name'); // product name
     if(name == undefined) return undefined;
 
-    // let el_merchant_uid = parser_common.get_data_tag_elem($, 'input', 'name', 'cartId'); // product name
-    // if(el_merchant_uid == undefined) return undefined;
+    let el_merchant_uid = parser_common.get_data_tag_elem($, 'input', 'name', 'cartId'); // product name
+    if(el_merchant_uid == undefined) return undefined;
     
-    // let merchant_uid = parser_common.get_elem_attr_value(el_merchant_uid, 'value');
-    // if(merchant_uid == undefined) return undefined;
-
-    let data_script = __get_data_script_from_checkout_page($);
-    if(data_script == undefined){
-        return undefined;
-    }
-
-    let data_script_contents = data_script.children[0].data;
-    let merchant_uid = data_script_contents.split('checkoutInfo.cartId = ')[1].split(';')[0].trim();
+    let merchant_uid = parser_common.get_elem_attr_value(el_merchant_uid, 'value');
+    if(merchant_uid == undefined) return undefined;
     merchant_uid += '_' + new Date().getTime();
 
     let user_code = parser_common.get_data_tag_value($, 'div', 'data-identity-code');
@@ -86,26 +78,6 @@ function set_const_kakaopay_prepare_payload(payload){
     return common.merge_object(payload, const_payload);
 }
 
-
-function __get_data_script_from_checkout_page($){
-
-    let scripts = $('script:not([src])').get();
-    let data_script = undefined;
-
-    for(var i = 0 ; i < scripts.length; i++){
-        
-        let script = scripts[i];
-        if('type' in script.attribs) continue;
-            
-        let script_data = script.children[0].data;
-
-        if(script_data.includes('var checkoutInfo = {};') == false) continue;
-        data_script = script;
-        break;
-    }
-
-    return data_script;
-}
 
 
 module.exports.parse_kakaopay_prepare_payload_from_checkout_page = parse_kakaopay_prepare_payload_from_checkout_page;
