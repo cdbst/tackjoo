e3a36a051d62d3556d56061d011cfb294b6cfb64
cdbst <65590329+cdbst@users.noreply.github.com>
2022-01-09 00:57:05 +0900

    ISSUE #106: 의류상품을 구매하지 못하는 현상 (#116)
    * 신발이 아닌 물건 구매가 가능하도록 코드 수정.

--- src/api/task_utils.js
+++ src/api/task_utils.js
@@ -43,24 +43,51 @@ module.exports.is_valid_product_info_to_tasking = (product_info) =>{
     return has_quantity;
 }
 
+function judge_appropreate_non_shoe_size_info(product_info, task_info){
+
+    let size_info_list_has_quantity = [];
+
+    if(product_info.sell_type == common.SELL_TYPE.draw){
+
+        let target_size_info = product_info.size_info_list.find((size_info) => { return size_info.name === task_info.size_name});
+        if(target_size_info != undefined) return target_size_info;
+
+        size_info_list_has_quantity = product_info.size_info_list;
+
+    }else{
+        size_info_list_has_quantity = product_info.size_info_list.filter((size_info) => { return size_info.quantity > 0} );
+    }
+
+    if(size_info_list_has_quantity.length == 0){ // 재고가 하나도 없는 상태임.
+        return undefined;
+    }
+
+    const target_size_info = size_info_list_has_quantity.find((size_info) => { return size_info.name === task_info.size_name} );
+    if(target_size_info !== undefined) return target_size_info;
+
+    return size_info_list_has_quantity[Math.floor((size_info_list_has_quantity.length - 1) / 2)];
+}
+
 module.exports.judge_appropreate_size_info = (product_info, task_info) =>{
 
     if(product_info.size_info_list == undefined){
         return undefined;
     }
 
-    let size_info_len = product_info.size_info_list.length;
-
-    if(size_info_len == 0){
+    if(product_info.size_info_list.length == 0){
         return undefined;
     }
 
-    let compare_size = (a, b) => {
-        let _a = parseInt(a.replace(/D/gi, ''));
-        let _b = parseInt(b.replace(/D/gi, ''));
-
-        return _a - _b
+    if(task_info.size_name.replace(/\D/g, '') === ''){ // 신발이 아님.
+        return judge_appropreate_non_shoe_size_info(product_info, task_info);
     }
+
+    const compare_size = (a, b) => {
+        const _a = parseInt(a.replace(/\D/gi, ''));
+        const _b = parseInt(b.replace(/\D/gi, ''));
+
+        return _a - _b;
+    };
 
     let size_info_list_has_quantity = [];
 
