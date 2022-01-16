8eebc1f97806730e08347d72d2d44d2655f5bb5c
cdbst <parkhyeonwook@gmail.com>
2022-01-16 10:32:25 +0900

    .

--- src/task.js
+++ src/task.js
@@ -73,8 +73,8 @@ async function main(browser_context, task_info, product_info, billing_info, sett
 
     if(browser_context.proxy_info !== undefined || //프록시가 셋팅 되어 있으면 무조건 로그인을 다시한다.
         browser_context.login_date === undefined ||  //로그인을 아직 하지 않은 상태라면 로그인을 시도한다.
-        is_login_session_expired) // 로그인이 되어 있지만, 로그인 세션이 만료됐다면 다시 로그인을 시도한다.
-        {
+        is_login_session_expired){ // 로그인이 되어 있지만, 로그인 세션이 만료됐다면 다시 로그인을 시도한다.
+        
         // STEP1.5 : Check validation of Task Information.
         global.MainThreadApiCaller.call('send_message', [common.TASK_STATUS.TRY_TO_LOGIN]);
         const login_result = await TaskUtils.login(browser_context);
