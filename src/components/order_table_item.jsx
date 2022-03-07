ada0358a5d06948007ecc8e3fb4b96b0b9a94a6b
cdbst <parkhyeonwook@gmail.com>
2022-03-08 02:22:34 +0900

    .

--- src/components/order_table_item.jsx
+++ src/components/order_table_item.jsx
@@ -82,7 +82,7 @@ class OrderTableItem extends React.Component {
                             </button>
                         </div>
                         <div className="float-start button-wrapper-inner-table" title="주문 취소하기">
-                            <button type="button" className="btn btn-info" onClick={this.onClickCancelOrder.bind(this)}>
+                            <button type="button" className="btn btn-danger" onClick={this.onClickCancelOrder.bind(this)}>
                                 <img src="./res/img/trash-fill.svg" style={{width:24, height:24}}/>
                             </button>
                         </div>
