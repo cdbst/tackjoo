5507ff76422f23b1f62e619f9577d335fab90014
cdbst <65590329+cdbst@users.noreply.github.com>
2022-05-05 01:09:33 +0900

    Issue#338 : 페이코 결제시 잘못된 비밀번호를 입력하는 현상 (#341)
    * OCR engine worker를 여러개 사용하는 방식을 적용.
    * OCR engine의 worker 사용시 세마포어를 이용하여 자원 사용에 대한 소유권 관리 코드를 적용함.
    
    * payco 비밀번호 입력 방식 개선

--- src/api/ocr_engine.js
+++ src/api/ocr_engine.js
@@ -4,35 +4,53 @@ const log = require('electron-log');
 const { app } = require('electron');
 const path = require('path');
 
+const { Semaphore } = require("async-mutex");
+
 class OCREngine{
     constructor(){
+
+        this.worker_cnt = 5;
         
         this.__scheduler = Tesseract.createScheduler();
+        this.__worker_semaphore = new Semaphore(this.worker_cnt);
 
         const app_path = app.getAppPath();
 
         (async()=>{
-            const worker = Tesseract.createWorker({
-                logger: (m) => {
-                    log.warn(common.get_log_str('ocr_engine.js', 'tesseract-logger', m.status))
-                },
-                workerPath: path.join(app_path, 'tesseract', 'worker', 'worker.min.js')
-            });
-            await worker.load();
-            await worker.loadLanguage('eng');
-            await worker.initialize('eng');
-            await worker.setParameters({
-                tessedit_char_whitelist: '0123456789',
-                preserve_interword_spaces: '0',
-            });
 
-            this.__scheduler.addWorker(worker);
+            for(var i = 0; i < this.worker_cnt; i++){
+
+                const worker = Tesseract.createWorker({
+                    logger: (m) => {
+                        log.warn(common.get_log_str('ocr_engine.js', 'tesseract-logger', m.status))
+                    },
+                    workerPath: path.join(app_path, 'tesseract', 'worker', 'worker.min.js')
+                });
+                await worker.load();
+                await worker.loadLanguage('eng');
+                await worker.initialize('eng');
+                await worker.setParameters({
+                    tessedit_char_whitelist: '0123456789',
+                    preserve_interword_spaces: '0',
+                });
+    
+                this.__scheduler.addWorker(worker);
+            }
         })();
     }
 
     async get_text(image){
-        const { data: { text } } = await this.__scheduler.addJob('recognize', image);
-        return text;
+
+        let _release = undefined;
+        
+        try{
+            const [_, release] = await this.__worker_semaphore.acquire();
+            _release = release;
+            const { data: { text } } = await this.__scheduler.addJob('recognize', image);
+            return text;
+        }finally{
+            if(_release !== undefined) _release();
+        }
     }
 }
 
