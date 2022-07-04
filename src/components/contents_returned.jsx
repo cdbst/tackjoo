f3197004faf5184625360daec932c8ba3385edf3
cdbst <parkhyeonwook@gmail.com>
2022-07-04 23:22:43 +0900

    고장나있던 필터링 기능 고침

--- src/components/contents_returned.jsx
+++ src/components/contents_returned.jsx
@@ -24,7 +24,7 @@ class ContentsReturned extends React.Component {
 
         this.__ref_sel_product_name = React.createRef();
         this.__ref_sel_account_name = React.createRef();
-        this.__ref_sel_return_req_date = React.createRef();
+        this.__ref_sel_returned_date = React.createRef();
         this.__ref_sel_returned_status = React.createRef();
         this.__ref_load_btn = React.createRef();
 
@@ -99,34 +99,34 @@ class ContentsReturned extends React.Component {
 
     setFilters(returned_info_list){
 
-        let product_name_list = common.getValuesFromObjList(returned_info_list, 'name');
+        let product_name_list = common.getValuesFromObjList(returned_info_list, 'product_name');
         product_name_list.unshift('');
 
         let account_email_list = common.getValuesFromObjList(returned_info_list, 'account_email');
         account_email_list.unshift('');
 
-        let order_date_list = common.getValuesFromObjList(returned_info_list, 'date');
-        order_date_list.sort((a, b)=>{ 
+        let returned_date_list = common.getValuesFromObjList(returned_info_list, 'returned_date');
+        returned_date_list.sort((a, b)=>{ 
             if(a > b) return -1;
             else if(a < b) return 1;
             else return 0;
         });
-        order_date_list = order_date_list.map((date)=> common.get_formatted_date_str(date));
-        order_date_list = [...new Set(order_date_list)];
-        order_date_list.unshift('');
+        returned_date_list = returned_date_list.map((date)=> common.get_formatted_date_str(date));
+        returned_date_list = [...new Set(returned_date_list)];
+        returned_date_list.unshift('');
 
-        let order_status_list = common.getValuesFromObjList(returned_info_list, 'status');
-        order_status_list.unshift('');
+        let returned_status_list = common.getValuesFromObjList(returned_info_list, 'returned_status');
+        returned_status_list.unshift('');
 
         this.setState({
             opt_list_product_name : product_name_list,
             opt_list_account_email : account_email_list,
-            opt_list_return_req_date : order_date_list,
-            opt_list_returned_status : order_status_list,
+            opt_list_return_req_date : returned_date_list,
+            opt_list_returned_status : returned_status_list,
         }, () => {
             this.__ref_sel_product_name.current.setValue('');
             this.__ref_sel_account_name.current.setValue('');
-            this.__ref_sel_return_req_date.current.setValue('');
+            this.__ref_sel_returned_date.current.setValue('');
             this.__ref_sel_returned_status.current.setValue('');
         });
     }
@@ -151,7 +151,7 @@ class ContentsReturned extends React.Component {
         }, () => {
             this.__ref_sel_product_name.current.setValue('');
             this.__ref_sel_account_name.current.setValue('');
-            this.__ref_sel_return_req_date.current.setValue('');
+            this.__ref_sel_returned_date.current.setValue('');
             this.__ref_sel_returned_status.current.setValue('');
             if(__callback)__callback();
         });
@@ -161,14 +161,14 @@ class ContentsReturned extends React.Component {
         
         const cur_sel_product_name = this.__ref_sel_product_name.current.getSelectedOptionValue();
         const cur_sel_account_email = this.__ref_sel_account_name.current.getSelectedOptionValue();
-        const cur_sel_order_date = this.__ref_sel_return_req_date.current.getSelectedOptionValue();
-        const cur_sel_order_status = this.__ref_sel_returned_status.current.getSelectedOptionValue();
+        const cur_sel_returned_date = this.__ref_sel_returned_date.current.getSelectedOptionValue();
+        const cur_sel_returned_status = this.__ref_sel_returned_status.current.getSelectedOptionValue();
 
         const filtered_returned_info_list = this.returned_info_list.filter((returned_info) =>{
-            if( (cur_sel_product_name == '' || cur_sel_product_name == returned_info.name) &&
+            if( (cur_sel_product_name == '' || cur_sel_product_name == returned_info.product_name) &&
                 (cur_sel_account_email == '' || cur_sel_account_email == returned_info.account_email) &&
-                (cur_sel_order_date == '' || cur_sel_order_date == common.get_formatted_date_str(returned_info.date)) &&
-                (cur_sel_order_status == '' || cur_sel_order_status == returned_info.status)
+                (cur_sel_returned_date == '' || cur_sel_returned_date == common.get_formatted_date_str(returned_info.returned_date)) &&
+                (cur_sel_returned_status == '' || cur_sel_returned_status == returned_info.returned_status)
             ){
                 return true;
             }else{
@@ -214,22 +214,6 @@ class ContentsReturned extends React.Component {
 
     render() {
 
-        const test_returned_info = {
-            product_name : '에어 조던 4 테스트',
-            account_email : 'xxxx@yyyyy.com',
-            product_img_url : 'https://static-breeze.nike.co.kr/kr/ko_kr/cmsstatic/product/DH6927-061/8e8b57b9-fe9f-4f92-adfc-010fdd2c1939_primary.jpg',
-            product_model_id : 'XXXX-YYYY',
-            product_option : '300',
-            product_price : '234,000',
-            returned_number : '123123123123123123123123123',
-            order_id : '456789',
-            returned_quantity : '1',
-            returned_date : new Date(),
-            returned_status: '반품신청중',
-            is_cancelable: true,
-            _id : common.uuidv4(),
-        }
-
         return (
             <div className="tab-pane fade" id="returned" role="tabpanel" aria-labelledby={MenuBar.MENU_ID.RETURNED}>
                 <div className="container-fluid">
@@ -245,7 +229,7 @@ class ContentsReturned extends React.Component {
                             <LabelSelect ref={this.__ref_sel_product_name} label="상품" options={this.state.opt_list_product_name} label_col_class="col-md-2" select_col_class="col-md-10" h_on_change={this.onChangeOption.bind(this)}/>
                         </div>
                         <div className="col-md-3">
-                            <LabelSelect ref={this.__ref_sel_return_req_date} label="반품요청일" options={this.state.opt_list_return_req_date} h_on_change={this.onChangeOption.bind(this)}/>
+                            <LabelSelect ref={this.__ref_sel_returned_date} label="반품요청일" options={this.state.opt_list_return_req_date} h_on_change={this.onChangeOption.bind(this)}/>
                         </div>
                         <div className="col-md-2">
                             <LabelSelect ref={this.__ref_sel_returned_status} label="진행단계" options={this.state.opt_list_returned_status} label_col_class="col-md-5" select_col_class="col-md-7" h_on_change={this.onChangeOption.bind(this)}/>
@@ -270,22 +254,6 @@ class ContentsReturned extends React.Component {
                         </thead>
                         <tbody>
                             {this.state.returned_table_item_list}
-                            {/* <ReturnedTableItem
-                                account_col_width = {this.account_col_width}
-                                product_size_col_width = {this.product_size_col_width}
-                                product_price_col_width = {this.product_price_col_width}
-                                returned_date_col_width = {this.returned_date_col_width}
-                                actions_col_width = {this.actions_col_width}
-                                product_img_col_width = {this.product_img_col_width}
-                                product_model_id_col_width = {this.product_model_id_col_width}
-                                returned_number_col_width = {this.returned_number_col_width}
-                                returned_quantity_col_width = {this.returned_quantity_col_width}
-                                returned_status_col_width = {this.returned_status_col_width}
-                                product_name_col_width = {this.product_name_col_width}
-                                returned_info = {test_returned_info}
-                                h_update_returned_info = {this.updateReturnedInfo.bind(this)}
-                                key={test_returned_info._id}
-                            /> */}
                         </tbody>
                     </table>
                     </div>
